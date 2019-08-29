e96ca30af47be5f5b2909da3730fbe46017dd168
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-08-30 06:19:20 +0900

    delete sapaces

--- tests/porker_test.py
+++ tests/porker_test.py
@@ -447,11 +447,11 @@ class TestFulleHouse(unittest.TestCase):
         self.assertEqual(self.full_house.porker_hand, 'FullHouse')
 
     def test_check_is_True(self):
-        self.hand.hand =   [Card('♠︎', '3'),
-                            Card('♣︎', '3'),
-                            Card('♦︎', '5'),
-                            Card('♥', '5'),
-                            Card('♠︎', '5')]
+        self.hand.hand = [Card('♠︎', '3'),
+                          Card('♣︎', '3'),
+                          Card('♦︎', '5'),
+                          Card('♥', '5'),
+                          Card('♠︎', '5')]
         self.three_card.check(self.hand)
         self.one_pair.check(self.hand)       
         self.full_house.check(self.hand, self.one_pair.result , self.three_card.result) 
