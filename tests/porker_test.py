6d84f1b180108751c998a487a978e041a49e7d1c
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-08-31 08:30:55 +0900

    delete spaces

--- tests/porker_test.py
+++ tests/porker_test.py
@@ -453,7 +453,7 @@ class TestFulleHouse(unittest.TestCase):
                           Card('♥', '5'),
                           Card('♠︎', '5')]
         self.three_card.check(self.hand)
-        self.one_pair.check(self.hand)       
+        self.one_pair.check(self.hand)
         self.full_house.check(self.hand, self.one_pair.result , self.three_card.result) 
         self.assertEqual(self.full_house.result, True)
     
