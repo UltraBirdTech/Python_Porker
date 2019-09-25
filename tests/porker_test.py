2ca2211dd6ecbb877093f5c4883a30c3abb85fdc
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-09-26 05:58:39 +0900

    edit indent

--- tests/porker_test.py
+++ tests/porker_test.py
@@ -283,7 +283,7 @@ class TestStraightFlash(unittest.TestCase):
         self.straight.check(self.hand)
         self.straight_flash.check(self.hand,
                                   self.flash.result,
-                                    self.straight.result)
+                                  self.straight.result)
         self.assertEqual(self.straight_flash.result, True)
         self.assertEqual(self.straight_flash.porker_hand, 'RoyalStraightFlash')
         self.assertEqual(self.straight_flash.is_royal(self.hand), True)
