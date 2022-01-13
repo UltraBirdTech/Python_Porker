413d9e90d1838061864887c2d5a8e55755e0a435
UltraBirdTech <ultrabirdrepti@gmail.com>
2022-01-14 08:33:53 +0900

    delete comment

--- porker.py
+++ porker.py
@@ -532,12 +532,6 @@ class JokerOnePair(Pair, Joker):
     def check_conditions(self, hand):
         self.check_joker(hand)
         self.result = True # Joker を持っている場合は最低でもOnePairになるためTrueになる。
-        #if hand.is_joker():
-        #    self.result = True # Joker を持っている場合は最低でもOnePairになるためTrueになる。
-        #    return
-
-        # Joker が存在しない場合はError.
-        #raise NotIncludeJokerError()
 
 class Peke(PorkerHand):
     def __init__(self):
